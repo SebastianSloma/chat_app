1309957c982abfb658a282235cedeabd7baf7ef1
Sebastian <sebastiansloma@gmail.com>
2023-04-12 11:43:12 +0200

    added frontend setup

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,17 @@ app.use(cors({ origin: true }));
 
 app.post('/authenticate', async (req, res) => {
 	const { username } = req.body;
-	return res.json({ username: username, secret: 'sha256...' });
+
+	try {
+		const r = await axios.r(
+			'https://api.chatengine.io/users/',
+			{ username: username, secret: username, first_name: username },
+			{ headers: { 'Private-Key': '98782893-5a13-4d98-88b2-8a896b7d063f' } }
+		);
+		return res.status(r.status).json(r.data);
+	} catch (e) {
+		return res.status(e.response.status).json(e.response.data);
+	}
 });
 
 app.listen(3001);
